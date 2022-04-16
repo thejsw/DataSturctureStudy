6c268729d19031e78c33d892413c18f0ed12921d
thejsw <jswtncapetnc@gmail.com>
2022-04-16 18:56:06 +0900

    pythonDS Queue

--- week7_Queue/homework/w$w.py
+++ week7_Queue/homework/w$w.py
@@ -1,3 +1,30 @@
 from StackLinkedList import *
 
-class sentence
+class sentenceChecker:
+    def __init__(self):
+        self.__stack = LinkedStack()
+    
+    def is_included(self, s):
+        idx = s.find('$')
+
+        for i in range(idx):
+            self.__stack.push(s[i])
+        
+        for i in range(idx+1, len(s)):
+            prev = self.__stack.pop()
+            curr = s[i]
+
+            if prev != curr:
+                return False
+        
+        return self.__stack.isEmpty()
+
+if __name__ == "__main__":
+    s = 'abcde$edcba'
+    checker = sentenceChecker()
+    rv = checker.is_included(s)
+    if rv == True:
+        print("True")
+    else:
+        print("False")
+

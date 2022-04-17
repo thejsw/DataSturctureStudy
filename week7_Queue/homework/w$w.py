f18fffb991d44745d60b1349f9649b437d1fb355
thejsw <jswtncapetnc@gmail.com>
2022-04-17 21:34:43 +0900

    pythonDS Queue w$w

--- week7_Queue/homework/w$w.py
+++ week7_Queue/homework/w$w.py
@@ -1,3 +1,10 @@
+# find 함수 구현
+# $ 기준으로 split
+# 나눈 왼쪽 단어 len 확인
+# 왼쪽 - 0에서 시작, 1씩, len까지
+# 오른쪽 - len에서 시작, -1씩, 0까지
+# 계속 맞는지 확인, 아닐경우 바로 중단하고 False 리턴
+
 from StackLinkedList import *
 
 class sentenceChecker:

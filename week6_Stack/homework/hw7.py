56d7936b02a465d701a0df8ffc7e28bd5b9e2875
thejsw <jswtncapetnc@gmail.com>
2022-04-29 13:27:53 +0900

    pythonDS - stack으로 factorial 구현하기

--- week6_Stack/homework/hw7.py
+++ week6_Stack/homework/hw7.py
@@ -1,15 +1,34 @@
-# factorial
+from StackLinkedList import *
 
-s = 'abcd$dcba'
+# def fact(n):
+#     while n>0:
+#         # self.__stack.append(n)
+#         n -= 1
+#     result = 1
+#     while stack:
+#         # result *= stack.pop()
+#     # return result
 
-def fact(n):
-    stack = []
-    while n>0:
-        stack.append(n)
-        n -= 1
-    result = 1
-    while stack:
-        result *= stack.pop()
-    return result
+# print(fact(5))
 
-print(fact(5))
+class factorial:
+    def __init__(self):
+        self.__stack = LinkedStack()
+    
+    def fact(self, n):
+        idx = s.find('$')
+
+        while n>0:
+            self.__stack.push(n)
+            n-=1
+        result = 1
+
+        for i in range(n):
+            result *= self.__stack.pop()
+
+        return result
+
+if __name__ == "__main__":
+    f = factorial()
+    값 = f.fact(5)
+    print(값)

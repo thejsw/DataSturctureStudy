9c0d2ca50a38f80719fb3d86170ec38506759cfb
thejsw <jswtncapetnc@gmail.com>
2022-05-24 13:33:47 +0900

    pythonDS - BubbleSort

---
+++ week9_sort/study/ch2_BubbleSort.py
@@ -0,0 +1,16 @@
+# 2. 버블 정렬
+
+## 이웃한 두 수를 비교하여 제일 큰 원소를 오른쪽 끝으로 보냄
+## 시간복잡도: 세타(n*)
+
+A = [3, 4, 2, 1, 5]
+
+def BubbleSort(A):
+    for numElements in (len(A), 0, -1):
+        for i in range(numElements-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+    return A
+
+result = BubbleSort(A)
+print(result)
